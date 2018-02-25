8c9955f655dfc90978923d260b1b7918ba0f2805
Uzair Iqbal <uzair.qq@outlook.com>
2018-02-25 19:47:33 +0500

    created a function in app component like a nested component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,13 @@ import React, {
 import logo from './logo.svg';
 import './App.css';
 
+function SayHello() {
+  return <h1>heloo world uzair here</h1>
+}
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>hello world</h1>
-      </div>
+      <SayHello>hello</SayHello>
     );
   }
 }
