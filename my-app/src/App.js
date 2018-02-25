e8cf5f08691273f180d6bc9c82ae8f08152e7284
Uzair Iqbal <uzair.qq@outlook.com>
2018-02-25 19:55:45 +0500

    we can also call this function component like this

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function SayHello() {
-  return <h1>heloo world uzair here</h1>
+  return <h1>hello world</h1>
 }
 class App extends Component {
   render() {
     return (
-      <SayHello>hello</SayHello>
+      <SayHello />
     );
   }
 }
