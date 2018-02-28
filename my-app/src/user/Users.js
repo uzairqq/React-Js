b801ec433738417367dcb1ac4a4c3bc05dce54a2
Uzair Iqbal <uzair.qq@outlook.com>
2018-02-28 17:46:22 +0500

    used states for dynamic instead of props

--- my-app/src/user/Users.js
+++ my-app/src/user/Users.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import User from './User';
 
 class Users extends Component {
+    state = {
+        users: [
+            { name: "uzair", age: 20 },
+            { name: "laraib", age: 30 },
+            { name: "ashar", age: 40 }
+        ],
+        title: "Users List"
+    };
     render() {
         return (
             <div>
-                <User age="29">Uzair</User>
-                <User age="30">laraib</User>
-                <User age="25">ashar</User>
+                <h1>{this.state.title}</h1>
+                <User age={this.state.users[0].age}>{this.state.users[0].name}</User>
+                <User age={this.state.users[1].age}>{this.state.users[1].name}</User>
+                <User age={this.state.users[2].age}>{this.state.users[2].name}</User>
             </div>
 
         );
