d298e685be507492f0e6f094b026bb2afb8588b5
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-01 12:52:24 +0500

    delete functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import Child from './parentToChild/child';
-import Parent from './parentToChild/parent';
+import User from './components/user'
 
 class App extends Component {
   state = {
-    title: 'Placeholder Title'
+    users: [
+      { name: "uzair", age: 20 },
+      { name: "laraib", age: 30 },
+      { name: "pasha", age: 40 }
+    ]
   }
-  changeTheWorld = (newTitle) => {
-    this.setState({
-      title: newTitle
-    })
+  DeleteUser = (index, e) => {
+    const users = Object.assign([], this.state.users);
+    users.splice(index, 1);
+    this.setState({ users: users });
   }
   render() {
     return (
       <div className="App">
-        <Parent doWhatEver={this.changeTheWorld.bind(this, 'uzair')} title={this.state.title} />
+        <ul>
+          {
+            this.state.users.map((user, index) => {
+              return (<User age={user.age} deleteEvent={this.DeleteUser.bind(this, index)}>{user.name}</User>)
+            })
+          }
+        </ul>
       </div>
     );
   }
