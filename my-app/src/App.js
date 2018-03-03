4a6fa964f540367bac2f9d7ce462ad711e0a2441
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-03 15:49:42 +0500

    used array instead of opening and closing div tag

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import User from './components/user'
+
+const Temp = () => {
+  return (
+    [
+      <div>Hi</div>,
+      <div>Hello</div>
+    ]
+  )
+}
 
 class App extends Component {
-  state = {
-    users: [
-      { id: 'abcd', name: "uzair", age: 20 },
-      { id: 'efgh', name: "laraib", age: 30 },
-      { id: 'ijklm', name: "pasha", age: 40 }
-    ]
-  }
-  DeleteUser = (index, e) => {
-    const users = Object.assign([], this.state.users);
-    users.splice(index, 1);
-    this.setState({ users: users });
-  }
+
+
   render() {
     return (
       <div className="App">
-        <ul>
-          {
-            this.state.users.map((user, index) => {
-              return (<User age={user.age} key={user.id} deleteEvent={this.DeleteUser.bind(this, index)}>{user.name}</User>)
-            })
-          }
-        </ul>
+        <Temp />
       </div>
     );
   }
