7cb08159e802adc697a2de5abfbde2048d6db5c0
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-03 18:25:29 +0500

    used local way of using pure components that if the state value changes then it render the component and temp functional component... if value changes the render both components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -21,9 +21,15 @@ class App extends Component {
   componentDidMount() {
     setInterval(() => {
       this.setState(() => {
-        return { val: 1 }
+        return { val: Math.random() }
       })
     }, 2000)
+  }
+
+  shouldComponentUpdate(nextProp, nextState) {
+    console.log('nextState', nextState);
+    console.log('currentState', this.state);
+    return (this.state.val === nextState.val ? false : true)
   }
 
   render() {
