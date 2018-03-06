b0dab50d079d2934c81c3ed750f769d19ed84474
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-06 17:51:41 +0500

    added the create functionality of task management system

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
-
+import CreateTask from './components/create-task';
+const tasks = [
+  {
+    task: 'complete React Tutorial',
+    isCompleted: false
+  },
+  {
+    task: 'read a book',
+    isCompleted: true
+  },
+];
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      tasks
+    };
+  }
+  createTask(task) {
+    this.state.tasks.push({
+      task,
+      isCompleted: false
+    });
+    this.setState({ tasks: this.state.tasks })
+  }
+
   render() {
     return (
       <div>
         <h1>React ToDos App</h1>
+        <CreateTask
+          tasks={this.state.tasks}
+          createTask={this.createTask.bind(this)}
+        />
       </div>
     );
   }
