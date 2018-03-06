4e894861f01fc44abb0e6d232b0ed16407b39148
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-06 12:27:31 +0500

    using miltiple components and react strap

---
+++ my-app/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>In a new component! yaay</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
