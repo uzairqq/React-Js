3c7d93802840c2290cb4c0341255f76fb76e4e09
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-06 17:06:56 +0500

    starting task managment system from here

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Home from './components/Home';
+
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      homeLinking: "Products"
-    }
-  }
-  onGreet() {
-    alert('hello word from parent component');
-  }
-
-  onClickHeaderNameChange(newNames) {
-    this.setState({
-      homeLinking: newNames
-    })
-  }
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-10 col-xs-offset-1">
-            <Header homeLinks={this.state.homeLinking} />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-10 col-xs-offset-1">
-            <Home name="uzair" initiallAge={20} greet={this.onGreet} changeLink={this.onClickHeaderNameChange.bind(this)} />
-          </div>
-        </div>
+      <div>
+        <h1>React ToDos App</h1>
       </div>
     );
   }
