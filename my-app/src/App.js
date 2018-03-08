a4b68f07b73a8c177f283608560a816b36104f8d
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-08 14:14:13 +0500

    Empty Project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,78 +1,21 @@
 import React, { Component } from 'react';
-import TaskList from './components/task-list';
-import CreateTask from './components/create-task';
+import logo from './logo.svg';
+import './App.css';
 
-const tasks = [
-  {
-    task: 'complete React Tutorial',
-    isCompleted: false
-  },
-  {
-    task: 'read a book',
-    isCompleted: true
-  },
-  {
-    task: 'Wash Car',
-    isCompleted: false
-  },
-
-];
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      tasks
-    };
-  }
-
-  createTask(task) {
-    this.state.tasks.push({
-      task,
-      isCompleted: false
-    });
-    this.setState({ tasks: this.state.tasks })
-  }
-
-  toggleTask(task) {
-    let t = this.state.tasks;
-    const foundTask = t.find(tsk => tsk.task === task)
-    foundTask.isCompleted = !foundTask.isCompleted;
-    this.setState({ tasks: this.state.tasks });
-
-  }
-
-  saveTask(oldTask, newTask) {
-    let t = this.state.tasks;
-    const foundTask = t.find(tsk => tsk.task === oldTask)
-    foundTask.task = newTask;
-    this.setState({ tasks: this.state.tasks })
-  }
-
-  deleteTask(taskToDelete) {
-    let t = this.state.tasks;
-    const foundTask = t.find(tsk => tsk.task === taskToDelete)
-    t.remove(foundTask);
-    this.setState({ tasks: this.state.tasks });
-  }
-
+class App extends Component {
   render() {
     return (
-      <div>
-        <h1> React ToDos App</h1>
-        <CreateTask
-          tasks={this.state.tasks}
-          createTask={this.createTask.bind(this)}
-        />
-        <TaskList
-          tasks={this.state.tasks}
-          toggleTask={this.toggleTask.bind(this)}
-          saveTask={this.saveTask.bind(this)}
-          deleteTask={this.deleteTask.bind(this)}
-        />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
-
     );
   }
 }
+
+export default App;
