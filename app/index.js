16dd632679ee321b1de2a8f9840a27cd7389bbef
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-14 17:01:02 +0500

    used proptypes for strong data typing

--- app/index.js
+++ app/index.js
@@ -1,37 +1,31 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-class ProfilePic extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={'https://photo.fb.com/' + this.props.username} />
-            </div>
-        )
-    }
-}
-class ProfileLink extends React.Component {
-    render() {
-        return (
-            <div>
-                <a href={'https://www.fb.com/' + this.props.username}>
-                    {this.props.username}
-                </a>
-            </div>
-        )
-    }
-}
-
+import PropTypes from 'prop-types';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <ProfilePic username={this.props.username} />
-                <ProfileLink username={this.props.username} />
+                <img src={this.props.img}
+                    alt='Avatar'
+                    style={{ width: 100, height: 100 }} />
+                <h1>Name:{this.props.name}</h1>
+                <h3>UserName:{this.props.username}</h3>
             </div>
         )
     }
 }
-ReactDOM.render(<App username="uzairqq" />, document.getElementById('app'));
+App.PropTypes = {
+    img: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired
+}
+
+
+
+ReactDOM.render(<App
+    name="Uzair Iqbal"
+    username="tyler"
+    img={'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'}
+/>, document.getElementById('app'));
