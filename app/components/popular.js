21df8da3991120b903a58d37254499de98a613db
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-15 15:42:06 +0500

    selected the langugae

--- app/components/popular.js
+++ app/components/popular.js
@@ -6,17 +6,27 @@ class Popular extends React.Component {
         this.state = {
             selectedLanguage: 'All'
         }
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+    updateLanguage(lang) {
+        this.setState(function () {
+            return {
+                selectedLanguage: lang
+            }
+        })
     }
     render() {
         var languages = ['All', 'Javascript', 'Ruby', 'Python', 'CSS', 'Java', 'Asp.Net'];
         return (
-
             <ul className="languages">
-                {languages.map(function (lang, index) {
+                <p>Select Language:{this.state.selectedLanguage}</p>
+                {languages.map(function (lang) {
                     return (
-                        <li key={lang}>{lang}</li>
+                        <li
+                            onClick={this.updateLanguage.bind(null, lang)}
+                            key={lang}>{lang}</li>
                     )
-                })}
+                }, this)}
             </ul>
         )
     }
