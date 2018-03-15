a2f415f1c8fc5b4b34a7a2939470f73677cd3d1b
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-15 14:14:38 +0500

    created components folder and created there an app.js file and used that app.js by calling in index.js

--- app/index.js
+++ app/index.js
@@ -2,52 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import propTypes from 'prop-types'
+import App from '../app/components/app';
 
-class App extends Component {
-    render() {
-
-        var friend = this.props.list.filter(function (user) {
-            return user.friend === true
-        });
-        var nonFriend = this.props.list.filter((user) => {
-            return user.friend === false
-        });
-        return (
-            <div>
-                <h1>Friends</h1>
-                <ul>
-                    {/*Create an <li> for every name in the list array who is also your friend*/}
-                    {friend.map(function (user) {
-                        return <li key={user.name}>{user.name}</li>
-                    })}
-                </ul>
-
-                <hr />
-
-                <h1> Non Friends </h1>
-                <ul>
-                    {/*Create an <li> for every name in the list array who is NOT your friend*/}
-                    {nonFriend.map(function (user) {
-                        return <li key={user.name}>{user.name}</li>
-                    })}
-                </ul>
-            </div>
-        )
-    }
-}
-App.propTypes = {
-    list: propTypes.arrayOf(propTypes.shape({
-        name: propTypes.string.isRequired,
-        friend: propTypes.bool.isRequired
-    }))
-}
-ReactDOM.render(<App
-    list={[
-        { name: 'Tyler', friend: true },
-        { name: 'Ryan', friend: true },
-        { name: 'Michael', friend: false },
-        { name: 'Mikenzi', friend: false },
-        { name: 'Jessica', friend: true },
-        { name: 'Dan', friend: false }]}
-
-/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
