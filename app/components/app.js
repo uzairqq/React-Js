b4e3149cb54347ce4950dbcd201ef009014204e6
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-18 19:30:04 +0500

    done with initial routing using react router and uses navlink instead of link because of styling and used exact keyword for focus on current selection

--- app/components/app.js
+++ app/components/app.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Popular from '../components/popular'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
+import Nav from '../components/nav';
 class App extends React.Component {
     render() {
         return (
             <Router >
                 <div className="container">
-                    <Route path="/popular" Component={Popular} />
+                    <Nav />
+                    <Route path="/popular" component={Popular} />
+                    <Route path="/battle" component={Popular} />
+
                 </div>
             </Router>
         )
