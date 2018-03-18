0862575589f60730ce3fa469405787a64ea5180c
Uzair Iqbal <uzair.qq@outlook.com>
2018-03-18 20:21:33 +0500

    created battle component

---
+++ app/components/Battle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Battle extends React.Component {
+    render() {
+        return (
+            <div><h1>Battle</h1></div>
+        )
+    }
+}
+
+export default Battle;
--- app/components/app.js
+++ app/components/app.js
@@ -3,6 +3,8 @@ import Popular from '../components/popular'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Nav from '../components/nav';
 import Home from './Home'
+import Battle from './Battle'
+
 class App extends React.Component {
     render() {
         return (
@@ -10,6 +12,7 @@ class App extends React.Component {
                 <div className="container">
                     <Nav />
                     <Route exact path="/" component={Home} />
+                    <Route exact path="/battle" component={Battle} />
                     <Route path="/popular" component={Popular} />
                 </div>
             </Router>
