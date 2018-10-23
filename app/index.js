90be1dc6831e9f396f13dfbcb547a2cfcc3cce33
Uzair Iqbal <uzair.qq@outlook.com>
2018-10-23 21:06:18 +0500

    New Tutorial and concepts

--- app/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import propTypes from 'prop-types'
-import App from '../app/components/app';
-
-ReactDOM.render(<App />, document.getElementById('app'));
