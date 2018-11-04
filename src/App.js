38d026f4e35cad7d74de3a157d79c0cb5b54197b
Uzair Iqbal <uzair.qq@outlook.com>
2018-11-05 00:48:46 +0500

    State And Props Mixed Example

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = { name: "uzair" };
-  }
-  render() {
+  state = {
+    users: [
+      { name: "uzair", age: 22, hobbies: "Cricket" },
+      { name: "laraib", age: 23, hobbies: "Football" },
+      { name: "sarmad", age: 25, hobbies: "Snooker" }
 
+    ]
+  };
+
+  render() {
     setTimeout(() => {
-      this.setState({ name: "pashasss" })
-    }, 4000);
+      this.setState({
+        users: [
+          { name: "uzair222", age: 222, hobbies: "Crickets" },
+          { name: "laraib33", age: 223, hobbies: "Footballs" },
+          { name: "sarmad44", age: 225, hobbies: "Snookers" }
+        ]
+      })
+    }, 1000);
     return (
       <div className="App">
+        <Person name={this.state.users[0].name} age={this.state.users[0].age}>Hobbies:{this.state.users[0].hobbies}</Person>
+        <Person name={this.state.users[1].name} age={this.state.users[1].age}>Hobbies:{this.state.users[1].hobbies}</Person>
+        <Person name={this.state.users[2].name} age={this.state.users[2].age}>Hobbies:{this.state.users[2].hobbies}</Person>
 
-        {/* <Person name="uzair" age="25" />
-        <Person name="ashar" age="29">Hobbies: Cricket</Person>
-        <Person name="shumail" age="33" /> */}
-        {this.state.name}
+
+
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>I'm a {props.name} and i am {props.age} years old </p>
-            <p>{props.children}</p>
+            <p>I'm a {props.name} and i am {props.age} years old And My{props.children}</p>
         </div>
     )
 }
