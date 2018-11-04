f2499367a6f01d0255e89e0818c5fdf46fd7bdfe
Uzair Iqbal <uzair.qq@outlook.com>
2018-11-04 20:35:01 +0500

    Used States  and setState

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { name: "uzair" };
+  }
   render() {
+
+    setTimeout(() => {
+      this.setState({ name: "pashasss" })
+    }, 4000);
     return (
       <div className="App">
-        <h1>Hi , I Am A React App Developer</h1>
-        <Person name="uzair" age="25" />
-        <Person name="ashar" age="29">Hobbies: Cricket</Person>
-        <Person name="shumail" age="33" />
 
+        {/* <Person name="uzair" age="25" />
+        <Person name="ashar" age="29">Hobbies: Cricket</Person>
+        <Person name="shumail" age="33" /> */}
+        {this.state.name}
       </div>
     );
   }
