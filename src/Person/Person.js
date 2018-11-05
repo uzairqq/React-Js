4cc21c0f785aa809dc005e43b6ff0fe259f3ccf7
Uzair Iqbal <uzair.qq@outlook.com>
2018-11-05 12:56:38 +0500

    Added StypesSheet To the  component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './Person.css';
+
 
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.methodClick} >I'm a {props.name} and i am {props.age} years old And My{props.children}</p>
             Name:<input type="text" onChange={props.changedName} value={props.name} />
         </div>
