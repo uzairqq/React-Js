647404de9d181824ef85ae8b21f0f500fe6b3af1
Uzair Iqbal <uzair.qq@outlook.com>
2018-11-05 11:54:03 +0500

    understand the bind method

--- src/App.js
+++ src/App.js
@@ -11,10 +11,11 @@ class App extends Component {
 
     ]
   };
-  SwitchNameMethod = () => {
+  newName = "uzairsssssss!!"
+  SwitchNameMethod = (newName) => {
     this.setState({
       users: [
-        { name: "uzairs", age: 222, hobbies: "Crickets" },
+        { name: newName, age: 222, hobbies: "Crickets" },
         { name: "laraibs", age: 233, hobbies: "Footballs" },
         { name: "sarmads", age: 225, hobbies: "Snookers" }
       ]
@@ -24,7 +25,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <button onClick={this.SwitchNameMethod}>Switch Here</button>
+        <button onClick={this.SwitchNameMethod.bind(this, this.newName)}>Switch Here</button>
         <Person methodClick={this.SwitchNameMethod} name={this.state.users[0].name} age={this.state.users[0].age}>Hobbies:{this.state.users[0].hobbies}</Person>
         <Person name={this.state.users[1].name} age={this.state.users[1].age}>Hobbies:{this.state.users[1].hobbies}</Person>
         <Person name={this.state.users[2].name} age={this.state.users[2].age}>Hobbies:{this.state.users[2].hobbies}</Person>
