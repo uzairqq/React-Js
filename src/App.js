37076229afae3acd439ddecdbdb1b5bda2b51dae
Uzair Iqbal <uzair.qq@outlook.com>
2018-11-05 11:49:58 +0500

    passing method references as props

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class App extends Component {
         { name: "uzairs", age: 222, hobbies: "Crickets" },
         { name: "laraibs", age: 233, hobbies: "Footballs" },
         { name: "sarmads", age: 225, hobbies: "Snookers" }
-
       ]
     })
   }
@@ -26,7 +25,7 @@ class App extends Component {
     return (
       <div className="App">
         <button onClick={this.SwitchNameMethod}>Switch Here</button>
-        <Person name={this.state.users[0].name} age={this.state.users[0].age}>Hobbies:{this.state.users[0].hobbies}</Person>
+        <Person methodClick={this.SwitchNameMethod} name={this.state.users[0].name} age={this.state.users[0].age}>Hobbies:{this.state.users[0].hobbies}</Person>
         <Person name={this.state.users[1].name} age={this.state.users[1].age}>Hobbies:{this.state.users[1].hobbies}</Person>
         <Person name={this.state.users[2].name} age={this.state.users[2].age}>Hobbies:{this.state.users[2].hobbies}</Person>
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>I'm a {props.name} and i am {props.age} years old And My{props.children}</p>
+            <p onClick={props.methodClick} >I'm a {props.name} and i am {props.age} years old And My{props.children}</p>
         </div>
     )
 }
