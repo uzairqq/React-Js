3240d0651703152318e6379e7094d68c717ea3cf
Uzair Iqbal <uzair.qq@outlook.com>
2022-01-27 03:57:54 +0500

    Footer Done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>footer workss</div>;
+  let footerStyle={
+    position: "sticky",
+    height: "20vh",
+    width: "100%"
+  }
+  return (
+    <footer  className="bg-dark text-light py-3" style={footerStyle}>
+      <p className="text-center">
+        Copyright &copy; MyTodoList.com
+      </p>
+    </footer>
+  )
 };
 
 export default Footer;
