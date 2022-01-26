210c7b7c67cb01a2f2720a63bc59e0041057b0a4
Uzair Iqbal <uzair.qq@outlook.com>
2022-01-27 03:59:11 +0500

    Footer Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Footer = () => {
   let footerStyle={
-    position: "sticky",
-    height: "20vh",
+    position: "absolute",
+    top: "100vh",
     width: "100%"
   }
   return (
-    <footer  className="bg-dark text-light py-3" style={footerStyle}>
+    <footer className="bg-dark text-light py-3" style={footerStyle}>
       <p className="text-center">
         Copyright &copy; MyTodoList.com
       </p>
