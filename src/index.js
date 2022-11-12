171423495cecb1555bd0a5e5b5f7af5863ab8603
Uzair Iqbal <uzair.qq@outlook.com>
2022-11-12 18:40:48 +0500

    Print Javascript Variables in JSX

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
 const App = () => {
+  let message = "Bye There";
+  if (Math.random() > 0.5) {
+    message = "Hi There";
+  }
   return (
     <div>
-      <h1>hello world</h1>
+      <h1>{message}</h1>
     </div>
   );
 };
