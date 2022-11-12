fc6a0750a03dec82d72d664fdaab301ce550c6e5
Uzair Iqbal <uzair.qq@outlook.com>
2022-11-12 22:20:37 +0500

    Installed Faker for Fake Images. Comments UI and App.js Skeleton

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-
-const element = document.getElementById("root");
-
-const root = ReactDOM.createRoot(element);
+import ReactDOM from "react-dom";
+import { faker } from "@faker-js/faker";
 
 const App = () => {
-  return <textarea autoFocus />;
+  return (
+    <div className="ui container comments">
+      <div class="comment">
+        <a class="avatar">
+          <img alt="avatar" src={faker.image.image()} />
+        </a>
+        <div class="content">
+          <a href="/" class="author">
+            Matt
+          </a>
+          <div class="metadata">
+            <span class="date">Today at 5:42PM</span>
+          </div>
+          <div class="text">Dude, this is awesome. Thanks so much</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-root.render(<App />);
+ReactDOM.render(<App />, document.querySelector("#root"));
