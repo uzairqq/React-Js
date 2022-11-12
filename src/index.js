5a5a8f451dbfe1bd9d9f3ba52a788c5be938f69f
Uzair Iqbal <uzair.qq@outlook.com>
2022-11-12 20:17:29 +0500

    Customizing Elements with Props

--- src/index.js
+++ src/index.js
@@ -6,15 +6,11 @@ const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
 const App = () => {
-  let message = "Bye There";
-  if (Math.random() > 0.5) {
-    message = "Hi There";
-  }
-  return (
-    <div>
-      <h1>{message}</h1>
-    </div>
-  );
+  const type = "number";
+  const maxVal = 5;
+  const min = -4;
+
+  return <input type={type} max={maxVal} min={min} />;
 };
 
 root.render(<App />);
