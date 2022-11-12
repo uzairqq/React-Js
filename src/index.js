6dfc7c4afaba4d107592859bbe89e2767be10b7f
Uzair Iqbal <uzair.qq@outlook.com>
2022-11-12 20:33:21 +0500

    All Props Must Follow Camel Caase

--- src/index.js
+++ src/index.js
@@ -6,11 +6,7 @@ const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
 const App = () => {
-  const type = "number";
-  const maxVal = 5;
-  const min = -4;
-
-  return <input type={type} max={maxVal} min={min} />;
+  return <textarea autoFocus />;
 };
 
 root.render(<App />);
